968686c5e9b1b3df2150093dbccb82d6e8831004
Amirhossein Mafi <amir77mafi@gmail.com>
2023-09-19 10:50:37 +0330

    add nvim mason packages

--- init.lua
+++ init.lua
@@ -586,10 +586,6 @@ end
 --  If you want to override the default filetypes that your language server will attach to you can
 --  define the property 'filetypes' to the map in question.
 local servers = {
-  -- clangd = {},
-  -- gopls = {},
-  -- pyright = {},
-  -- rust_analyzer = {},
   tsserver = {},
   html = {},
   tailwindcss = {
@@ -603,7 +599,15 @@ local servers = {
       }
     }
   },
-
+  cspell = {},
+  bashls = {},
+  cssls = {},
+  eslint_d = {},
+  prettierd = {},
+  shellcheck = {},
+  shfmt = {},
+  sqlfmt = {},
+  sqlls = {},
   lua_ls = {
     Lua = {
       workspace = { checkThirdParty = false },
@@ -642,6 +646,7 @@ mason_lspconfig.setup_handlers {
 local cmp = require 'cmp'
 local luasnip = require 'luasnip'
 require('luasnip.loaders.from_vscode').lazy_load()
+-- require('luasnip.loaders.from_vscode').lazy_load({ paths = { "./snippets" } })
 luasnip.config.setup {}
 
 cmp.setup {
